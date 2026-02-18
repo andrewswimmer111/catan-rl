8803b1099f4a9cfc1e540f18387c996b57fdac12
Andrew Li <al576@duke.edu>
2026-02-18 13:44:54 -0500

    Feature: Created EdgeCollection, updated board to use collections

--- src/engine/board/board.py
+++ src/engine/board/board.py
@@ -1,6 +1,6 @@
 from tile import Tile, TileCollection
 from vertex import Vertex, VertexCollection
-from edge import Edge
+from edge import Edge, EdgeCollection
 from math import sqrt
 
 class Board:
@@ -9,24 +9,25 @@ class Board:
     """
 
     def __init__(self):
-        self.tiles: TileCollection = []
+        self.tiles: TileCollection = TileCollection()
         self.vertices: VertexCollection = VertexCollection()
-        self.edges = []
+        self.edges: EdgeCollection = EdgeCollection()
 
     def create_regular_board(self):
-        self.tiles = self.create_tiles()
-        self.vertices = self.create_vertices()
-        self.edges = self.create_edges()
+        self.create_tiles()
+        self.create_vertices()
+        self.create_edges()
 
+
+
+    # ---- Helpers Below -----
 
     def create_tiles(self, R=2):
-        tiles = []
         for q in range(-R, R + 1):
             r_min = max(-R, -q - R)
             r_max = min(R, -q + R)
             for r in range(r_min, r_max + 1):
-                tiles.append(Tile(q, r))
-        return tiles
+                self.tiles.append(Tile(q, r))
     
 
     def create_vertices(self):
@@ -56,6 +57,7 @@ class Board:
                     vertex = created_vertices[vertex_coord]
                 else:
                     vertex = Vertex(vertex_coord[0], vertex_coord[1])
+                    self.vertices.append(vertex)
                     created_vertices[vertex_coord] = vertex
 
                 tile.assign_vertex(vertex, i)
@@ -92,5 +94,3 @@ class Board:
                     tile_below = TileCollection.get_by_axial_coords(q, r + 1)
                     if tile_below is not None:
                         self.edges.append(Edge(vertex, tile_below.vertices[4]))
-
-            
--- src/engine/board/edge.py
+++ src/engine/board/edge.py
@@ -7,3 +7,23 @@ class Edge:
 
     def __init__(self, v1, v2):
         self.vertices: list[Vertex] = [v1, v2]
+
+class EdgeCollection:
+    """
+    Docstring for EdgeCollection
+    """
+
+    def __init__(self):
+        self.collection: list[Edge] = []
+
+    def __iter__(self):
+        return iter(self.collection)
+    
+    def __getitem__(self, index):
+        return self.collection[index]
+    
+    def __len__(self):
+        return len(self.collection)
+    
+    def append(self, tile):
+        self.collection.append(tile)
