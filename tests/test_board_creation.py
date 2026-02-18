2ea18efb1ca10faed053b64d141659200acc0226
Andrew Li <al576@duke.edu>
2026-02-18 15:28:59 -0500

    Fix: added pyproject.toml - fixes testing import errors

--- tests/test_board_creation.py
+++ tests/test_board_creation.py
@@ -11,4 +11,11 @@ def regular_board():
 def test_board_creation_throws_no_exceptions(regular_board):
     assert(True)
 
+def test_correct_num_tiles(regular_board):
+    assert(len(regular_board.tiles) == 19)
 
+def test_correct_num_vertices(regular_board):
+    assert(len(regular_board.vertices) == 54)
+
+def test_correct_num_edges(regular_board):
+    assert(len(regular_board.edges) == 72)
