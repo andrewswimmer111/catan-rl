4e3b95d3bdecffe7968773bf7a90fea293ae5cee
Andrew Li <al576@duke.edu>
2026-02-20 21:11:20 -0500

    Feature: added resource stacks

--- src/engine/resources/resources.py
+++ src/engine/resources/resources.py
@@ -6,3 +6,30 @@ class Resource(Enum):
     WHEAT = "wheat"
     SHEEP = "sheep"
     ORE = "ore"
+
+
+class ResourceStack():
+
+    def __init__(self, resource: Resource, number: int):
+        self.resource = resource
+        self.number = number
+
+    def check_withdrawal_amount(self, amount):
+        if self.number >= amount:
+            return True
+        return False
+    
+    def withdraw(self, amount):
+        if self.check_withdrawal_amount():
+            self.number -= amount
+            # assign amount
+        else:
+            print("Invalid amount of resources left")
+
+    def add(self, amount):
+        self.number += amount
+
+
+class ResourceDistributer():
+    # distributes the correct amount of resources
+    # maybe identifies how many 
