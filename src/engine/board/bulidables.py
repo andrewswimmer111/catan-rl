7b233af12b949f34bc230e6867604ec132768184
Andrew Li <al576@duke.edu>
2026-02-20 21:32:02 -0500

    Feature: Created buildable classes

---
+++ src/engine/board/bulidables.py
@@ -0,0 +1,20 @@
+class Building:
+    def __init__(self, player, vertex):
+        self.player = player
+        self.vertex = vertex
+
+
+class Settlement(Building):
+    vp = 1
+    collection_amount = 1
+
+
+class City(Building):
+    vp = 2
+    collection_amount = 2
+
+
+class Road:
+    def __init__(self, player, edge):
+        self.player = player
+        self.edge = edge
